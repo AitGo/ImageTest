1cf8bc1b03a12ee60f4e4fff4caff21ea3e5c4d9
liu <ly490099@163.com>
2019-07-03 18:17:10 +0800

    修改图片转视频，增加视频音频合并

--- app/src/main/java/com/liu/imagetest/MainActivity.java
+++ app/src/main/java/com/liu/imagetest/MainActivity.java
@@ -2,6 +2,11 @@ package com.liu.imagetest;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.media.MediaCodec;
+import android.media.MediaExtractor;
+import android.media.MediaFormat;
+import android.media.MediaMuxer;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,6 +19,8 @@ import com.googlecode.javacv.cpp.opencv_core;
 import com.liu.imagetest.utils.FileUtils;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.ByteBuffer;
 
 import static com.googlecode.javacv.cpp.opencv_highgui.cvLoadImage;
 
@@ -21,6 +28,9 @@ public class MainActivity extends AppCompatActivity {
 
     private Button btn_start;
     public static final String IMAGE_TYPE = ".png";
+
+    private MediaMuxer mediaMuxer;
+    private static final String SDCARD_PATH = Environment.getExternalStorageDirectory().getAbsolutePath() + "/ScreenRecord/temp" + File.separator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
      */
     private void imageToMp4() {
         // 生成的新文件名
-        String newFileName = "test_" + System.currentTimeMillis() + ".mp4";
+        final String newFileName = "test_" + System.currentTimeMillis() + ".mp4";
         // 保存的路径
         String temp = null;
         final double frameRate = 0.5;
@@ -52,6 +62,7 @@ public class MainActivity extends AppCompatActivity {
             e1.printStackTrace();
         }
         final String savePath = temp;
+        final String finalTemp = temp;
         new Thread() {
             @Override
             public void run() {
@@ -85,11 +96,102 @@ public class MainActivity extends AppCompatActivity {
                     Log.d("test", "录制完成....");
                     // 录制结束
                     recorder.stop();
+                    File file = new File(finalTemp);
+                    Log.d("test", file.getName() + "....");
+                    combineVideo(newFileName,"audio.mp3");
                 } catch (FileUtils.NoSdcardException | FrameRecorder.Exception e) {
                     e.printStackTrace();
                 }
             }
         }.start();
+    }
+
+
+    /**
+     * 音频，视频合成
+     */
+    private void combineVideo(String videoName,String audioName) {
+        try {
+            MediaExtractor videoExtractor = new MediaExtractor();
+            videoExtractor.setDataSource(SDCARD_PATH + videoName);
+            MediaFormat videoFormat = null;
+            int videoTrackIndex = -1;
+            int videoTrackCount = videoExtractor.getTrackCount();
+            for (int i = 0; i < videoTrackCount; i++) {
+                videoFormat = videoExtractor.getTrackFormat(i);
+                String mimeType = videoFormat.getString(MediaFormat.KEY_MIME);
+                if (mimeType.startsWith("video/")) {
+                    videoTrackIndex = i;
+                    break;
+                }
+            }
+            MediaExtractor audioExtractor = new MediaExtractor();
+            audioExtractor.setDataSource(SDCARD_PATH + audioName);
+            MediaFormat audioFormat = null;
+            int audioTrackIndex = -1;
+            int audioTrackCount = audioExtractor.getTrackCount();
+            for (int i = 0; i < audioTrackCount; i++) {
+                audioFormat = audioExtractor.getTrackFormat(i);
+                String mimeType = audioFormat.getString(MediaFormat.KEY_MIME);
+                if (mimeType.startsWith("audio/")) {
+                    audioTrackIndex = i;
+                    break;
+                }
+            }
+            videoExtractor.selectTrack(videoTrackIndex);
+            audioExtractor.selectTrack(audioTrackIndex);
+            MediaCodec.BufferInfo videoBufferInfo = new MediaCodec.BufferInfo();
+            MediaCodec.BufferInfo audioBufferInfo = new MediaCodec.BufferInfo();
+            mediaMuxer = new MediaMuxer(SDCARD_PATH + "/output", MediaMuxer.OutputFormat.MUXER_OUTPUT_MPEG_4);
+            int writeVideoTrackIndex = mediaMuxer.addTrack(videoFormat);
+            int writeAudioTrackIndex = mediaMuxer.addTrack(audioFormat);
+            mediaMuxer.start();
+            ByteBuffer byteBuffer = ByteBuffer.allocate(500 * 1024);
+            long sampleTime = 0;
+            {
+                videoExtractor.readSampleData(byteBuffer, 0);
+                if (videoExtractor.getSampleFlags() == MediaExtractor.SAMPLE_FLAG_SYNC) {
+                    videoExtractor.advance();
+                }
+                videoExtractor.readSampleData(byteBuffer, 0);
+                long secondTime = videoExtractor.getSampleTime();
+                videoExtractor.advance();
+                long thirdTime = videoExtractor.getSampleTime();
+                sampleTime = Math.abs(thirdTime - secondTime);
+            }
+            videoExtractor.unselectTrack(videoTrackIndex);
+            videoExtractor.selectTrack(videoTrackIndex);
+            while (true) {
+                int readVideoSampleSize = videoExtractor.readSampleData(byteBuffer, 0);
+                if (readVideoSampleSize < 0) {
+                    break;
+                }
+                videoBufferInfo.size = readVideoSampleSize;
+                videoBufferInfo.presentationTimeUs += sampleTime;
+                videoBufferInfo.offset = 0;
+                videoBufferInfo.flags = videoExtractor.getSampleFlags();
+                mediaMuxer.writeSampleData(writeVideoTrackIndex, byteBuffer, videoBufferInfo);
+                videoExtractor.advance();
+            }
+            while (true) {
+                int readAudioSampleSize = audioExtractor.readSampleData(byteBuffer, 0);
+                if (readAudioSampleSize < 0) {
+                    break;
+                }
+                audioBufferInfo.size = readAudioSampleSize;
+                audioBufferInfo.presentationTimeUs += sampleTime;
+                audioBufferInfo.offset = 0;
+                audioBufferInfo.flags = videoExtractor.getSampleFlags();
+                mediaMuxer.writeSampleData(writeAudioTrackIndex, byteBuffer, audioBufferInfo);
+                audioExtractor.advance();
+            }
+            mediaMuxer.stop();
+            mediaMuxer.release();
+            videoExtractor.release();
+            audioExtractor.release();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
